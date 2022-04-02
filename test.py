733768640b31dd1f1d8a81b9b8c2378269e4b632
fazlullahb0 <fazlullahb0@gmail.com>
2022-04-02 12:43:07 +0530

    API

--- test.py
+++ test.py
@@ -1,5 +1,49 @@
-def test(a,b):
-    print(a+b)
-    return a+b
+from flask import Flask, request, jsonify
 
-test(10,5)
+
+app = Flask(__name__)
+
+
+@app.route('/xyz',methods=['GET','POST'])
+def test():
+   if request.method == 'POST':
+       a = request.json['num1']
+       b = request.json['num2']
+       result = a+b
+       return jsonify(str(result))
+
+
+@app.route('/abc/sudh',methods=['GET','POST'])
+def test1():
+    if request.method == 'POST':
+       a = request.json['num3']
+       b = request.json['num4']
+       result = a+b
+       return jsonify(str(result))
+
+@app.route('/abc/sudh/kumar',methods=['GET','POST'])
+def test2():
+    if request.method == 'POST':
+       a = request.json['num5']
+       b = request.json['num6']
+       result = a+b
+       return jsonify(str(result))
+
+@app.route('/abc/xyz',methods=['GET','POST'])
+def test3():
+    if request.method == 'POST':
+       a = request.json['num7']
+       b = request.json['num8']
+       result = a+b
+       return jsonify(str(result))
+
+@app.route('/faiz/xyz',methods=['GET','POST'])
+def test4():
+    if request.method == 'POST':
+       a = request.json['num9']
+       b = request.json['num10']
+       result = a+b
+       return jsonify(str(result))
+
+if __name__ == '__main__':
+    app.run()
